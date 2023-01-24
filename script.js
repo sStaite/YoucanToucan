33a3f0bb6896d16d83c2aee232dbd5c0c16d9358
sStaite <seamus.staite@gmail.com>
2023-01-24 16:09:02 +1100

    is there a toucan now?

--- script.js
+++ script.js
@@ -14,18 +14,12 @@ var game = new Phaser.Game(config);
 
 function preload ()
 {
-	this.load.image('sky', 'assets/sky.png');
-    this.load.image('ground', 'assets/platform.png');
-    this.load.image('star', 'assets/star.png');
-    this.load.image('bomb', 'assets/bomb.png');
-    this.load.spritesheet('dude', 
-        'assets/dude.png',
-        { frameWidth: 32, frameHeight: 48 }
-    );
+	this.load.image('toucan', 'images/toucan.png')
 }
 
 function create ()
 {
+	this.add.image(400, 300, 'toucan');
 }
 
 function update ()
