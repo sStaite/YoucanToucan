4cceaac2d29ce5f425ec926bcb93118370a7ee22
sStaite <seamus.staite@gmail.com>
2023-02-01 18:33:34 +1100

    guessing system works, markov chain doesn't update

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+/**************************************************************************************/
 
 var config = {
     type: Phaser.AUTO,
@@ -11,16 +12,20 @@ var config = {
     }
 };
 
+/**************************************************************************************/
+
 var game = new Phaser.Game(config);
 var markov_array = new Array(4)
 
 const user_nums = new Array()
 const comp_nums = new Array()
 
+/**************************************************************************************/
+
 function preload ()
 {
-    this.load.image('zero', 'images/zero.png');
-    this.load.image('one', 'images/one.png');
+    this.load.image('zero', 'assets/zero.png');
+    this.load.image('one', 'assets/one.png');
     initialise_markov_array();
     change_coefficients(1, 1, 1, 1);
 }
@@ -30,25 +35,61 @@ function create ()
     var zero_sprite = this.add.sprite(40, 100, 'zero').setInteractive();
     var one_sprite = this.add.sprite(40, 40, 'one').setInteractive();
 
+    var user_text = this.add.text(100, 60, '_', { 
+        fontFamily: 'andale mono', 
+        fontSize: '20px'
+    });
+
+    var comp_text = this.add.text(100, 160, '_', { 
+        fontFamily: 'andale mono', 
+        fontSize: '20px'
+    });
+
+    var player_win_percent = this.add.text(700, 60, '_', { 
+        fontFamily: 'andale mono', 
+        fontSize: '20px'
+    });
+
     zero_sprite.on('pointerdown', function (pointer) {
-        user_nums.push(0);
         // some sort of markov array update here
+        make_guess();
+        update_markov_array();
+
+        // add 0 to user array
+        user_nums.push(0);
+
+        // update the text on screen
+        user_text.setText(user_nums.toString());
+        comp_text.setText(comp_nums.toString());
+        player_win_percent.setText(get_win_percent())
+
     });
 
     one_sprite.on('pointerdown', function (pointer) {
-        user_nums.push(1);
         // some sort of markov array update here
+        make_guess();
+        update_markov_array();
+
+        // add 1 to user array
+        user_nums.push(1);
+
+        // update the text on screen
+        user_text.setText(user_nums.toString());
+        comp_text.setText(comp_nums.toString());
+        player_win_percent.setText(get_win_percent())
+
     });
 }
 
 function update ()
 {   
-    console.log(user_nums);
+    //console.log(user_nums);
 }
+
+/**************************************************************************************/
 
 function initialise_markov_array () 
 {
-    var markov_array = new Array(4)
     for (let i = 0; i < markov_array.length; i++) {
             markov_array[i] = new Array(4).fill(0);
     }
@@ -75,15 +116,57 @@ function update_markov_array()
 
 function make_guess()
 {
-    // this only works when user_nums has two numbers in it
-    if (user_nums.length < 2) {
-        comp_nums[user_nums.length - 1] = Math.random(0, 1); // look up how this works
+    // Add a random number and return if there are less than 2 numbers.
+    if (user_nums.length <= 2) {
+        comp_nums.push(Math.round(Math.random())); 
+        return;
     }
 
-    // Get last two digits
-    if (user_nums[user_nums.length - 1] = 0)
-    user_nums.length
+    // Get index for row of markov array we are using
+    let index;
+
+    if (user_nums[user_nums.length - 1] == 0) { // last digit is 0
+        if (user_nums[user_nums.length - 2] == 0) { // last two digits are: 00
+            index = 0;
+        } else {                                    // last two digits are: 10
+            index = 1;
+        }
+    } else {
+        if (user_nums[user_nums.length - 2] == 0) { // last two digits are: 01
+            index = 2;
+        } else {                                    // last two digits are: 11
+            index = 3;
+        }
+    }
+
+    // Find the highest probability for where the markov chain will go next
+    let guess_index = markov_array[index].indexOf(Math.max(...markov_array[index]));
+    console.log(markov_array[index])
+
+    if (guess_index == 0 || guess_index == 1) {
+        comp_nums.push(0);
+    } else {
+        comp_nums.push(1);
+    }
+
+
+        
 }
+
+function get_win_percent()
+{   
+    player_wins = 0
+    for(let i = 0; i < user_nums.length; i++) {
+        if (user_nums[i] != comp_nums[i]) {
+            player_wins++;
+        }
+    }
+    return (player_wins * 100 / user_nums.length) + '%'; 
+}
+
+/**************************************************************************************/
+
+
 
 
 
